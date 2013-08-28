28273dfc148ae845133c7a5d563146e21bb27343
Jonatan Pålsson <jonatan.palsson@pelagicore.com>
2013-08-28 15:46:53 +0200

    ImageFileGenerator: Use higher values for F-Number

--- src/generators/ImageFileGenerator.py
+++ src/generators/ImageFileGenerator.py
@@ -58,7 +58,7 @@ class ImageFileGenerator (AbstractFileGenerator):
 		elif prop == "Model": return s.getSurname()
 		elif prop == "Copyright": return s.getSentence(5)
 		elif prop == "WhiteBalance": return choice(["AUTO", "MANUAL"])
-		elif prop == "Fnumber": return "1/%d" % randint(2,800)
+		elif prop == "Fnumber": return "1/%d" % randint(2,8)
 		elif prop == "Flash": return choice(["ON", "OFF"])
 		elif prop == "FocalLength": return randint(1,1000)
 		elif prop == "ExposureTime": return randint(1,1000)
